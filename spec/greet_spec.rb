1a365d99caa444090298757e140bc94f0a5ec32a
Ayoub El-hamdoon <aze@mastudejgkxkg9h.lan>
2023-02-14 15:27:05 +0000

    fixed typo

--- spec/greet_spec.rb
+++ spec/greet_spec.rb
@@ -3,6 +3,6 @@ require "greet"
 RSpec.describe "greet method" do
     it "greets name entered as an argument" do
         result = greet("Ayoub")
-        expect(result).to eq "Hello Ayoub!"
+        expect(result).to eq "Hello, Ayoub!"
     end
 end
