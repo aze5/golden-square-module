77f636bb83b2fe73ee52b7ec149331a890b99ffd
Ayoub El-hamdoon <aze@mastudejgkxkg9h.lan>
2023-02-15 16:37:03 +0000

    green stage for third test

--- challenges/lib/grammar_checker.rb
+++ challenges/lib/grammar_checker.rb
@@ -1,9 +1,9 @@
 def grammar_checker(text)
     punctuation = ['.', '!', '?']
-    if text[0] == text[0].upcase && punctuation.include?(text[-1])
-        return "Good grammar!"
-    elsif text.strip == ""
+    if text.strip == ""
         return "ERROR, your text is empty"
+    elsif text[0] == text[0].upcase && punctuation.include?(text[-1])
+        return "Good grammar!"
     else
         return "Your grammar is incorrect"
     end
