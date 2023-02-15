c5f5af237d8a5034c33970d1e48bb8e93223831d
Ayoub El-hamdoon <aze@mastudejgkxkg9h.lan>
2023-02-15 16:31:26 +0000

    pass first and second tests grammar_checker

--- challenges/lib/grammar_checker.rb
+++ challenges/lib/grammar_checker.rb
@@ -1,3 +1,8 @@
 def grammar_checker(text)
-    return "Your grammar is incorrect"
+    punctuation = ['.', '!', '?']
+    if text[0] == text[0].upcase && punctuation.include?(text[-1])
+        return "Good grammar!"
+    else
+        return "Your grammar is incorrect"
+    end
 end
