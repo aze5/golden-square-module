bbc274940d085f97bf693820d8cb79ed093b852b
Ayoub El-hamdoon <aze@mastudejgkxkg9h.lan>
2023-02-15 17:31:33 +0000

    red stage for second test includes_string

--- challenges/spec/includes_string_spec.rb
+++ challenges/spec/includes_string_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe "includes_string" do
         it "returns '#TODO not found' when passed 'hello WORLD'" do
             expect(includes_string("hello WORLD")).to eq "#TODO not found"
         end
+
+        it "returns '#TODO found at index: 12' when passed 'hello world #TODO'" do
+            expect(includes_string("hello world #TODO")).to eq "#TODO found at index: 12"
+        end
     end
 end
