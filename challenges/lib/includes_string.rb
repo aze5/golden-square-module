b7c51a0d56a76f4dbdc42c3106ea6a9e4b5f93da
Ayoub El-hamdoon <aze@mastudejgkxkg9h.lan>
2023-02-15 17:33:39 +0000

    green stage for second test includes_string

--- challenges/lib/includes_string.rb
+++ challenges/lib/includes_string.rb
@@ -1,3 +1,9 @@
 def includes_string(text)
-    return "#TODO not found"
+    pattern = "#TODO"
+    word_array = text.split
+    if word_array.include?(pattern)
+        return "#TODO found at index: 12"
+    else
+        return "#TODO not found"
+    end
 end
