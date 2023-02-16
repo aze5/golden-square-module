002a4ca280d37e31c8e3448379c09857b036df30
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-02-16 17:36:08 +0000

    red stage second test music_tracker

--- challenges/spec/music_tracker_spec.rb
+++ challenges/spec/music_tracker_spec.rb
@@ -13,4 +13,11 @@ describe MusicTracker do
     songs.add("song name2")
     expect(songs.view).to eq ["song name", "song name2"]
   end
+
+  it "remove a song from array if .remove(song) is called" do
+    songs = MusicTracker.new
+    songs.add("song name")
+    songs.remove("song name")
+    expect(songs.view).to eq []
+  end
 end
