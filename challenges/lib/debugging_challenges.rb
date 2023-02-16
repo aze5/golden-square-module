850e1cc81852285f25cca4ebf7567343e5fc65c1
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-02-16 14:24:29 +0000

    completed challenge for debugging 1

--- challenges/lib/debugging_challenges.rb
+++ challenges/lib/debugging_challenges.rb
@@ -11,11 +11,12 @@ def encode(plaintext, key)
     return ciphertext_chars.join
 end
 
-puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
+# puts encode("theswiftfoxjumpedoverthelazydog", "secretkey")
 # => "EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL"
 
 def decode(ciphertext, key)
     cipher = key.chars.uniq + (('a'..'z').to_a - key.chars)
+    print cipher.length
     plaintext_chars = ciphertext.chars.map do |char|
         cipher[char.ord - 65]
   end
@@ -23,5 +24,21 @@ def decode(ciphertext, key)
 end
 
 
-puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
+# puts decode("EMBAXNKEKSYOVQTBJSWBDEMBPHZGJSL", "secretkey")
 # => "theswiftfoxjumpedoverthelazydog"
+
+# challenge
+def get_most_common_letter(text)
+    counter = Hash.new(0)
+    text.chars.each { |char|
+        counter[char] += 1
+    }
+    counter_arr = counter.to_a.sort_by {|k, v| v }
+    return counter_arr[-1][-1]  if counter_arr[-1][-2] != " "
+    return counter_arr[-2][-2]
+end
+
+# Intended output:
+# 
+print get_most_common_letter("the roof, the roof, the roof is on fire!")
+# => "o"
