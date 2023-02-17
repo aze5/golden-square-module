492663415ec94d41d0b177f6d05a96fbdb4e3f8a
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-02-17 12:34:18 +0000

    red stage for Diary.count_words spec

--- challenges/spec/diary_integration_spec.rb
+++ challenges/spec/diary_integration_spec.rb
@@ -2,10 +2,19 @@ require "diary"
 require "diary_entry"
 
 describe "integration" do
-  it "returns word count" do
+  it "returns returns diary entries" do
     diary = Diary.new
     diary_entry = DiaryEntry.new("07/11/19", "contents")
     diary.add(diary_entry)
     expect(diary.all).to eq [diary_entry]
+  end
+
+  it "returns word count in diary" do
+    diary = Diary.new
+    diary_entry = DiaryEntry.new("07/11/19", "contents " * 5)
+    diary_entry2 = DiaryEntry.new("entry", "contents " * 10)
+    diary.add(diary_entry)
+    diary.add(diary_entry2)
+    expect(diary.count_words).to eq 15
   end
 end
