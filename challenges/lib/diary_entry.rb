5ca6cc17c801dc88e186cd5867a34ac428741ad1
Ayoub El-hamdoon <aze@MASTUDENT-Ayoub-Elhamdoon-W7JGKXKG9H.local>
2023-02-17 12:44:06 +0000

    pass DiaryEntry.reading_time spec

--- challenges/lib/diary_entry.rb
+++ challenges/lib/diary_entry.rb
@@ -16,10 +16,8 @@ class DiaryEntry
     return @contents.split.length
   end
 
-  def reading_time(wpm) # wpm is an integer representing
-                        # the number of words the user can read per minute
-    # Returns an integer representing an estimate of the reading time in minutes
-    # for the contents at the given wpm.
+  def reading_time(wpm) 
+    return self.count_words / wpm
   end
 
   def reading_chunk(wpm, minutes) # `wpm` is an integer representing the number
--- challenges/spec/diary_entry_spec.rb
+++ challenges/spec/diary_entry_spec.rb
@@ -15,5 +15,9 @@ describe DiaryEntry do
     diary_entry = DiaryEntry.new("entry1", "contents " * 10)
     expect(diary_entry.count_words).to eq 10
   end
-
+  
+  it "returns reading time" do
+    diary_entry = DiaryEntry.new("entry1", "contents " * 100)
+    expect(diary_entry.reading_time(50)).to eq 2
+  end
 end
